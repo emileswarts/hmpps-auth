1d48b7b208761ba81498dcb0161c9c197395b56f
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-11-03 13:51:43 +0000

    DT-2813 Handle 400 errors on change password (#1063)
    * DT-2813 Handle 400 errors on change password
    
    * DT-2813 Handle 400 errors on change password (PR comments)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/service/NomisUserApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/service/NomisUserApiService.kt
@@ -1,18 +1,36 @@
 package uk.gov.justice.digital.hmpps.oauth2server.nomis.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.security.core.AuthenticationException
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.oauth2server.security.PasswordValidationFailureException
+import uk.gov.justice.digital.hmpps.oauth2server.security.ReusedPasswordException
 
 @Service
 class NomisUserApiService(
   @Qualifier("nomisWebClient") private val webClient: WebClient,
+  private val objectMapper: ObjectMapper,
 ) {
   fun changePassword(username: String, password: String) {
     webClient.put().uri("/users/{username}/change-password", username)
       .bodyValue(password)
       .retrieve()
       .toBodilessEntity()
+      .onErrorResume(WebClientResponseException::class.java) {
+        errorWhenBadRequest(it) { content ->
+          val error = objectMapper.readValue<PasswordChangeError>(content)
+          when (error.errorCode) {
+            1001 -> ReusedPasswordException()
+            else -> PasswordValidationFailureException()
+          }
+        }
+      }
       .block()
   }
 
@@ -30,3 +48,13 @@ class NomisUserApiService(
       .block()
   }
 }
+
+fun <T> errorWhenBadRequest(
+  exception: WebClientResponseException,
+  errorMapper: (content: String) -> AuthenticationException
+): Mono<T> =
+  if (exception.rawStatusCode == BAD_REQUEST.value()) Mono.error(errorMapper(exception.responseBodyAsString)) else Mono.error(
+    exception
+  )
+
+data class PasswordChangeError(val errorCode: Int? = 0)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/service/NomisUserApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/service/NomisUserApiServiceTest.kt
@@ -1,17 +1,26 @@
 package uk.gov.justice.digital.hmpps.oauth2server.nomis.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException.InternalServerError
 import uk.gov.justice.digital.hmpps.oauth2server.resource.IntegrationTest
 import uk.gov.justice.digital.hmpps.oauth2server.resource.NomisApiMockServer
 import uk.gov.justice.digital.hmpps.oauth2server.resource.NomisExtension
+import uk.gov.justice.digital.hmpps.oauth2server.security.PasswordValidationFailureException
+import uk.gov.justice.digital.hmpps.oauth2server.security.ReusedPasswordException
+import java.net.HttpURLConnection
 
 @ExtendWith(NomisExtension::class)
 class NomisUserApiServiceTest : IntegrationTest() {
@@ -21,20 +30,130 @@ class NomisUserApiServiceTest : IntegrationTest() {
   @Qualifier("nomisWebClient")
   lateinit var webClient: WebClient
 
+  @Autowired
+  lateinit var objectMapper: ObjectMapper
+
   private lateinit var nomisService: NomisUserApiService
 
   @BeforeEach
   fun setUp() {
-    nomisService = NomisUserApiService(webClient)
+    nomisService = NomisUserApiService(webClient, objectMapper)
   }
 
-  @Test
-  fun `changePassword`() {
-    nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2")
-    nomisApi.verify(
-      putRequestedFor(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password"))
-        .withRequestBody(equalTo("helloworld2"))
-    )
+  @DisplayName("changePassword")
+  @Nested
+  inner class ChangePassword {
+    @Test
+    fun `it will succeed when API is OK`() {
+      nomisApi.stubFor(
+        WireMock.put(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password")).willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpURLConnection.HTTP_OK)
+        )
+      )
+
+      nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2")
+      nomisApi.verify(
+        putRequestedFor(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password"))
+          .withRequestBody(equalTo("helloworld2"))
+      )
+    }
+
+    @Test
+    fun `it will throw an exception for unexpected errors`() {
+      nomisApi.stubFor(
+        WireMock.put(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password")).willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(
+              """
+                {
+                  "status": 500,
+                  "userMessage": "NullPointerException",
+                  "developerMessage": "NullPointerException"
+                }
+              """.trimIndent()
+            )
+            .withStatus(HttpURLConnection.HTTP_INTERNAL_ERROR)
+        )
+      )
+
+      assertThatThrownBy { nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2") }
+        .isInstanceOf(InternalServerError::class.java)
+    }
+
+    @Test
+    fun `it will throw a PasswordValidationFailureException for simple validation failures`() {
+      nomisApi.stubFor(
+        WireMock.put(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password")).willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(
+              """
+                {
+                    "status": 400,
+                    "errorCode": 1000,
+                    "userMessage": "Validation failure: changePassword.password: Password must consist of alphanumeric characters only and a minimum of 14 chars, and max 30 chars",
+                    "developerMessage": "changePassword.password: Password must consist of alphanumeric characters only and a minimum of 14 chars, and max 30 chars"
+                }
+              """.trimIndent()
+            )
+            .withStatus(HttpURLConnection.HTTP_BAD_REQUEST)
+        )
+      )
+
+      assertThatThrownBy { nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2") }
+        .isInstanceOf(PasswordValidationFailureException::class.java)
+    }
+
+    @Test
+    fun `it will throw a PasswordValidationFailureException for NOMIS validation failures`() {
+      nomisApi.stubFor(
+        WireMock.put(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password")).willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(
+              """
+                {
+                    "status": 400,
+                    "errorCode": 1002,
+                    "userMessage": "Password is not valid and has been rejected by NOMIS due to ORA-20001:  Password must contain at least 1 numeric character. Please re-enter password.\nORA-06512: at \"OMS_OWNER.TAG_ERROR\", line 169\nORA-06512: at \"OMS_OWNER.TAG_ERROR\", line 85\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 838\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 867\n",
+                    "developerMessage": "Password is not valid and has been rejected by NOMIS due to ORA-20001:  Password must contain at least 1 numeric character. Please re-enter password.\nORA-06512: at \"OMS_OWNER.TAG_ERROR\", line 169\nORA-06512: at \"OMS_OWNER.TAG_ERROR\", line 85\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 838\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 867\n"
+                }              
+              """.trimIndent()
+            )
+            .withStatus(HttpURLConnection.HTTP_BAD_REQUEST)
+        )
+      )
+
+      assertThatThrownBy { nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2") }
+        .isInstanceOf(PasswordValidationFailureException::class.java)
+    }
+
+    @Test
+    fun `it will throw a ReusedPasswordException when NOMIS detects password has been used before`() {
+      nomisApi.stubFor(
+        WireMock.put(urlEqualTo("/users/NOMIS_PASSWORD_RESET/change-password")).willReturn(
+          WireMock.aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody(
+              """
+                {
+                    "status": 400,
+                    "errorCode": 1001,
+                    "userMessage": "Password has been used before and was rejected by NOMIS due to ORA-20087: Password cannot be reused\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 870\n",
+                    "developerMessage": "Password has been used before and was rejected by NOMIS due to ORA-20087: Password cannot be reused\nORA-06512: at \"OMS_OWNER.OMS_UTILS\", line 870\n"
+                }                
+              """.trimIndent()
+            )
+            .withStatus(HttpURLConnection.HTTP_BAD_REQUEST)
+        )
+      )
+
+      assertThatThrownBy { nomisService.changePassword("NOMIS_PASSWORD_RESET", "helloworld2") }
+        .isInstanceOf(ReusedPasswordException::class.java)
+    }
   }
 
   @Test
