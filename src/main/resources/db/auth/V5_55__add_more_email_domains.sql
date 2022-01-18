d300db69a8f8b535a399570e52de4fbd86578082
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-01-18 17:31:59 +0000

    HAAR-15: addition email domains for interventions (#1146)

---
+++ src/main/resources/db/auth/V5_55__add_more_email_domains.sql
@@ -0,0 +1,8 @@
+INSERT INTO email_domain (email_domain_id, name, description)
+VALUES (newid(), '%stockportwomenscentre.co.uk', 'STOCKPORTWOMENSCENTRE'),
+        (newid(), '%urbanoutreach.co.uk', 'URBANOUTREACH'),
+        (newid(), '%petrus.org.uk', 'PETRUS'),
+        (newid(), '%jigsawhomes.org.uk', 'JIGSAWHOMES'),
+        (newid(), '%salfordfoundation.org.uk', 'SALFORDFOUNDATION'),
+        (newid(), '%partnersofprisoners.co.uk', 'PARTNERSOFPRISONERS');
+
