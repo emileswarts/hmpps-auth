a2ba6d6ecb434f77f80760d49954b7859110010b
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2022-05-04 16:08:34 +0100

    SDI-86 user self serve account creation (& remove user authentication) (#1249)
    * SDI-86 User self-serve account creation (& remove UserAuthentication)
    
    Develop Script to migrate check-my-diary auth data
    
    * SDI-86 User self-serve account creation (& remove UserAuthentication)
    
    Develop Script to migrate check-my-diary auth data
    
    * SDI-86 User self-serve account creation (& remove UserAuthentication)
    
    Add rate limit and reorder updates

--- src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/MigrateCMD2FA.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/MigrateCMD2FA.kt
@@ -1,149 +1,294 @@
 package uk.gov.justice.digital.hmpps.oauth2server
 
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.jdbc.datasource.SingleConnectionDataSource
 import java.sql.ResultSet
+import java.time.LocalDate
 import java.util.UUID
+
+const val UPDATE_USER_CONTACT = """update user_contact set details=? where user_id = ? and type = ?"""
+const val UPDATE_MFA_PREFERENCE = """update users set mfa_preference=? where user_id = ?"""
 
 /**
  * Temporary migration program to copy check-my-diary 2FA details from the CMD database into auth
  */
-class MigrateCMD2FA {
+class MigrateCMD2FA(
+  val cmd: JdbcTemplate,
+  val auth: JdbcTemplate,
+  val insertIntoUsers: SimpleJdbcInsert,
+  val insertIntoUserContact: SimpleJdbcInsert,
+  val migrate: Boolean = false,
+  val users: String = "",
+  val pauseMillis: Long = 0L,
+) {
 
   companion object {
 
-    // kc -n check-my-diary-prod port-forward port-forward-pod 5436:5432
-    val cmdUrl: String = System.getenv("CMD_URL")
-    val cmdUsername: String = System.getenv("CMD_USERNAME")
-    val cmdPassword: String = System.getenv("CMD_PASSWORD")
-
-    // kc -n hmpps-auth-preprod port-forward port-forward-pod 5433:5432
-    val authUrl: String = System.getenv("AUTH_URL")
-    val authUsername: String = System.getenv("AUTH_USERNAME")
-    val authPassword: String = System.getenv("AUTH_PASSWORD")
-
-    val cmd = JdbcTemplate(SingleConnectionDataSource(cmdUrl, cmdUsername, cmdPassword, false))
-    val auth = JdbcTemplate(SingleConnectionDataSource(authUrl, authUsername, authPassword, false))
-    val authUsersMap = mutableMapOf<String, User>()
-
-    var inUsers = 0
-    var inContact = 0
-
     @JvmStatic
     fun main(args: Array<String>) {
+      // kc -n check-my-diary-prod port-forward port-forward-pod 5436:5432
+      val cmdUrl: String = System.getenv("CMD_URL")
+      val cmdUsername: String = System.getenv("CMD_USERNAME")
+      val cmdPassword: String = System.getenv("CMD_PASSWORD")
+
+      // kc -n hmpps-auth-preprod port-forward port-forward-pod 5433:5432
+      val authUrl: String = System.getenv("AUTH_URL")
+      val authUsername: String = System.getenv("AUTH_USERNAME")
+      val authPassword: String = System.getenv("AUTH_PASSWORD")
+
+      val migrateEnv: String = System.getenv("MIGRATE")
+      val users: String = System.getenv("USERS") // set as 'ALL' for all users, or comma separated
+      val pauseMillis: Long = System.getenv("RATE_LIMIT_MILLIS")?.run { this.toLong() } ?: 200L
+
+      val cmd = JdbcTemplate(SingleConnectionDataSource(cmdUrl, cmdUsername, cmdPassword, false))
+      val auth = JdbcTemplate(SingleConnectionDataSource(authUrl, authUsername, authPassword, false))
+      val insertIntoUsers = SimpleJdbcInsert(auth).withTableName("users")
+        .usingColumns(
+          "user_id",
+          "username",
+          "verified",
+          "enabled",
+          "source",
+          "mfa_preference"
+        )
+      val insertIntoUserContact = SimpleJdbcInsert(auth).withTableName("user_contact")
+
       try {
-        doMigrate()
+        val c =
+          MigrateCMD2FA(cmd, auth, insertIntoUsers, insertIntoUserContact, migrateEnv == "true", users, pauseMillis)
+        c.initMaps()
+
+        c.forAllCMDRows()
       } finally {
         cmd.dataSource?.connection?.close()
         auth.dataSource?.connection?.close()
-        print("Closed connections. inUsers=$inUsers inContact=$inContact")
+        println("Closed connections.")
       }
     }
+  }
 
-    private fun doMigrate() {
+  val authUsersMap = mutableMapOf<String, User>()
+  var authUserContactsMap: Map<UUID, List<Contact>>? = null
 
-      auth.query(
-        """select 
-                u.user_id as "userId",
-                u.username,
-                COALESCE(u.email, '') as email,
-                u.verified,
-                u.mfa_preference as "mfaPreference"
-              from users u""",
-      ) { r, _ ->
-        User(
-          userId = UUID.fromString(r.getString("userId")),
-          username = r.getString("username"),
-          email = r.getString("email"),
-          verified = r.getBoolean("verified"),
-          mfaPreference = r.getString("mfaPreference"),
+  var inUsers = 0
+  var inContact = 0
+
+  fun initMaps() {
+    auth.query(
+      """select 
+                    u.user_id as "userId",
+                    u.username,
+                    COALESCE(u.email, '') as email,
+                    u.mfa_preference as "mfaPreference"
+                  from users u""",
+    ) { r, _ ->
+      User(
+        userId = UUID.fromString(r.getString("userId")),
+        username = r.getString("username"),
+        email = r.getString("email"),
+        mfaPreference = r.getString("mfaPreference"),
+      )
+    }
+      .forEach { authUsersMap[it.username] = it!! }
+
+    authUserContactsMap = auth.query(
+      """select user_id as "userId", type, details, verified from user_contact""",
+    ) { r, _ ->
+      Contact(
+        UUID.fromString(r.getString("userId")),
+        r.getString("type"),
+        r.getString("details"),
+        r.getBoolean("verified")
+      )
+    }
+      .groupBy { it.userId }
+
+    println("populated maps.")
+  }
+
+  fun forAllCMDRows() {
+    val userList = users
+      .split(",")
+      .joinToString(",", "(", ")") { "'${it.trim()}'" }
+    val whereClause = if (users == "ALL") "" else """ where "QuantumId" in $userList"""
+    cmd.query(
+      """select 
+                 "QuantumId",
+                 "EmailAddress",
+                 "Sms",
+                 "UseEmailAddress",
+                 "UseSms",
+                 "LastLoginDateTime"
+               from "UserAuthentication" $whereClause""",
+      if (migrate) ::doMigrate else ::doReport,
+    )
+    println("inUsers=$inUsers inContact=$inContact")
+  }
+
+  fun showPreferences(quantumId: String) {
+    cmd.query(
+      """select snooze_until, coalesce(email,'') as email, coalesce(sms,'') as sms, comm_pref
+          from user_preference where quantum_id = ?""",
+      { r, _ ->
+        Preferences(
+          r.getDate("snooze_until")?.toLocalDate(),
+          r.getString("email"),
+          r.getString("sms"),
+          r.getString("comm_pref")
         )
+      },
+      quantumId.uppercase()
+    ).firstOrNull()?.also {
+      if (it.hasData()) {
+        println("  user_preference = $it")
       }
-        .forEach { authUsersMap[it.username] = it!! }
+    }
+  }
 
-      val authUserContactsMap = auth.query(
-        """select user_id as "userId", type, details, verified from user_contact""",
-      ) { r, _ ->
-        Contact(
-          UUID.fromString(r.getString("userId")),
-          r.getString("type"),
-          r.getString("details"),
-          r.getBoolean("verified")
-        )
-      }
-        .groupBy { it.userId }
+  fun doReport(cmdResult: ResultSet) {
+    val quantumId = cmdResult.getString("QuantumId")
+    val emailAddress = cmdResult.getString("EmailAddress")
+    val sms = cmdResult.getString("Sms")
+    val useEmailAddress = cmdResult.getBoolean("UseEmailAddress")
+    val useSms = cmdResult.getBoolean("UseSms")
+    val lastLoginDate = cmdResult.getDate("LastLoginDateTime")?.toLocalDate()
 
-      cmd.query(
-        """select 
-            "QuantumId",
-            "EmailAddress",
-            "Sms",
-            "UseEmailAddress",
-            "UseSms",
-            "LastLoginDateTime"
-          from "UserAuthentication" """,
+    if (authUsersMap[quantumId.uppercase()] == null) {
+      println("missing from users: $quantumId")
+    } else {
+      val authUser = authUsersMap[quantumId.uppercase()]!!
+      inUsers++
 
-      ) { cmdResult: ResultSet ->
-        val quantumId = cmdResult.getString("QuantumId")
-        val emailAddress = cmdResult.getString("EmailAddress")
-        val sms = cmdResult.getString("Sms")
-        val useEmailAddress = cmdResult.getBoolean("UseEmailAddress")
-        val useSms = cmdResult.getBoolean("UseSms")
-        val lastLoginDate = cmdResult.getDate("LastLoginDateTime")?.toLocalDate()
+      authUserContactsMap!![authUser.userId]?.also { authUserContacts ->
+        inContact++
 
-        authUsersMap[quantumId.uppercase()]?.also { authUser ->
-          inUsers++
+        // println("$quantumId  ${authUser.userId.toString()}")
+        // showMainEmail(useEmailAddress, authUser, emailAddress, quantumId, authUserContacts)
 
-          // println(quantumId)
-
-          authUserContactsMap[authUser.userId]?.also { authUserContacts ->
-            inContact++
-
-            // println("$quantumId  ${authUser.userId.toString()}")
-            // showMainEmail(useEmailAddress, authUser, emailAddress, quantumId, authUserContacts)
-
-            authUserContacts.forEach {
-              if (it.type == "SECONDARY_EMAIL" && authUser.mfaPreference == "SECONDARY_EMAIL") {
-                if (useEmailAddress && it.verified && emailAddress.uppercase() != it.details.uppercase()) {
-                  println("$quantumId Secondary Email clash: CMD=$emailAddress, AUTH=${it.details} verified=${it.verified}, mfaPreference=${authUser.mfaPreference}, last CMD login=$lastLoginDate")
-                }
-              } else if (it.type == "MOBILE_PHONE" && authUser.mfaPreference == "TEXT") {
-                if (useSms && it.verified && sms != it.details.replace("+44", "0")) {
-                  println("$quantumId SMS clash: CMD=$sms, AUTH=${it.details}, verified=${it.verified}, mfaPreference=${authUser.mfaPreference}, last CMD login=$lastLoginDate")
-                }
-              }
+        authUserContacts.forEach {
+          if (it.type == "SECONDARY_EMAIL" && authUser.mfaPreference == "SECONDARY_EMAIL") {
+            if (useEmailAddress && it.verified && emailAddress.uppercase() != it.details.uppercase()) {
+              println("$quantumId Secondary Email clash: CMD=$emailAddress, AUTH=${it.details}, mfaPreference=${authUser.mfaPreference}, last CMD login=$lastLoginDate")
+              showPreferences(quantumId)
+            }
+          } else if (it.type == "MOBILE_PHONE" && authUser.mfaPreference == "TEXT") {
+            if (useSms && it.verified && sms != it.details.replace("+44", "0")) {
+              println("$quantumId SMS clash: CMD=$sms, AUTH=${it.details}, mfaPreference=${authUser.mfaPreference}, last CMD login=$lastLoginDate")
+              showPreferences(quantumId)
             }
           }
         }
       }
     }
+  }
 
-    private fun showMainEmail(
-      useEmailAddress: Boolean,
-      authUser: User,
-      emailAddress: String,
-      quantumId: String,
-      authUserContacts: List<Contact>
-    ) {
-      if (useEmailAddress && authUser.mfaPreference == "EMAIL" && emailAddress.uppercase() != authUser.email.uppercase()) {
-        print("$quantumId Main Email difference: CMD=$emailAddress, AUTH=${authUser.email}, mfaPreference=${authUser.mfaPreference}")
-        val secondaryEmail = authUserContacts.filter { it.type == "SECONDARY_EMAIL" }
-        if (secondaryEmail.isNotEmpty()) {
-          val first = secondaryEmail.first()
-          println("  SECONDARY_EMAIL exists=${first.details} verified=${first.verified} match=${first.details.uppercase() == emailAddress.uppercase()}")
-        } else {
-          println()
+  fun doMigrate(cmdResult: ResultSet) {
+    val quantumId = cmdResult.getString("QuantumId")
+    val emailAddress = cmdResult.getString("EmailAddress")
+    val sms = cmdResult.getString("Sms")
+    val useEmailAddress = cmdResult.getBoolean("UseEmailAddress")
+    val useSms = cmdResult.getBoolean("UseSms")
+    if (useEmailAddress || useSms) {
+      // give email precedence when both are true
+      val mfaPreference = if (useEmailAddress) "SECONDARY_EMAIL" else "TEXT"
+
+      val newUUID = UUID.randomUUID()
+      val authUser = authUsersMap[quantumId.uppercase()]
+        ?: insertIntoUsers.execute( // .executeAndReturnKeyHolder(
+          mapOf(
+            "user_id" to newUUID.toString(),
+            "username" to quantumId.uppercase(),
+            "verified" to true,
+            "enabled" to true,
+            "source" to "nomis",
+            "mfa_preference" to mfaPreference,
+          )
+        ).run {
+          User(
+            newUUID,
+            // this.getKeyAs(UUID::class.java)!!,
+            quantumId.uppercase(), "", mfaPreference
+          )
         }
+
+      val userId = authUser.userId
+
+      val authUserContacts = authUserContactsMap!![userId]
+      val smsAuth = authUserContacts?.firstOrNull { it.type == "MOBILE_PHONE" }?.run { details }
+      val smsExistsInAuth = smsAuth != null
+      val emailAuth = authUserContacts?.firstOrNull { it.type == "SECONDARY_EMAIL" }?.run { details }
+      val emailExistsInAuth = emailAuth != null
+
+      // Do any inserting first
+
+      if (!emailExistsInAuth && emailAddress != null && emailAddress.isNotBlank()) {
+        insertIntoUserContact.execute(
+          mapOf(
+            "user_id" to userId,
+            "type" to "SECONDARY_EMAIL",
+            "details" to emailAddress,
+            "verified" to useEmailAddress
+          )
+        )
+      }
+      if (!smsExistsInAuth && sms != null && sms.isNotBlank()) {
+        insertIntoUserContact.execute(
+          mapOf(
+            "user_id" to userId,
+            "type" to "MOBILE_PHONE",
+            "details" to sms,
+            "verified" to useSms
+          )
+        )
+      }
+
+      // update carefully!
+
+      val emailVerifiedAuth = authUserContacts?.any { it.type == "SECONDARY_EMAIL" && it.verified } ?: false
+      val smsVerifiedAuth = authUserContacts?.any { it.type == "MOBILE_PHONE" && it.verified } ?: false
+
+      if (useSms && smsExistsInAuth && !smsVerifiedAuth && sms != smsAuth) {
+        auth.update(UPDATE_USER_CONTACT, sms, userId, "MOBILE_PHONE")
+      }
+      if (useEmailAddress && emailExistsInAuth && !emailVerifiedAuth && emailAddress != emailAuth) {
+        auth.update(UPDATE_USER_CONTACT, emailAddress, userId, "SECONDARY_EMAIL")
+      }
+      if (authUser.mfaPreference != mfaPreference) {
+        auth.update(UPDATE_MFA_PREFERENCE, mfaPreference, userId)
+      }
+      Thread.sleep(pauseMillis!!)
+    }
+  }
+
+  private fun showMainEmail(
+    useEmailAddress: Boolean,
+    authUser: User,
+    emailAddress: String,
+    quantumId: String,
+    authUserContacts: List<Contact>
+  ) {
+    if (useEmailAddress && authUser.mfaPreference == "EMAIL" && emailAddress.uppercase() != authUser.email.uppercase()) {
+      print("$quantumId Main Email difference: CMD=$emailAddress, AUTH=${authUser.email}, mfaPreference=${authUser.mfaPreference}")
+      val secondaryEmail = authUserContacts.filter { it.type == "SECONDARY_EMAIL" }
+      if (secondaryEmail.isNotEmpty()) {
+        val first = secondaryEmail.first()
+        println("  SECONDARY_EMAIL exists=${first.details} verified=${first.verified} match=${first.details.uppercase() == emailAddress.uppercase()}")
+      } else {
+        println()
       }
     }
   }
+}
+
+data class Preferences(val snoozeUntil: LocalDate?, val email: String, val sms: String, val commPref: String) {
+  fun hasData() = email.isNotEmpty() || sms.isNotEmpty()
 }
 
 data class User(
   val userId: UUID,
   val username: String,
   val email: String,
-  val verified: Boolean,
   val mfaPreference: String,
 )
 
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/MigrateCMD2FATest.kt
@@ -0,0 +1,387 @@
+package uk.gov.justice.digital.hmpps.oauth2server
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
+import java.sql.ResultSet
+import java.util.UUID
+import java.util.concurrent.atomic.AtomicReference
+
+class MigrateCMD2FATest {
+  private val cmd: JdbcTemplate = mock()
+  private val auth: JdbcTemplate = mock()
+  private val insertIntoUsers: SimpleJdbcInsert = mock()
+  private val insertIntoUserContact: SimpleJdbcInsert = mock()
+  private val script = MigrateCMD2FA(cmd, auth, insertIntoUsers, insertIntoUserContact)
+
+  val UUID1 = UUID.fromString("3b9969ca-c57a-11ec-9d64-0242ac120002")
+  val UUID2 = UUID.fromString("9cb54de0-cad0-11ec-9d64-0242ac120002")
+  val UUID3 = UUID.fromString("9cb5504c-cad0-11ec-9d64-0242ac120002")
+
+  @Test
+  fun `Do Nothing if no 2FA in CMD`() {
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", false, false)
+    )
+
+    verify(auth, never()).update(any<String>(), any<String>(), any<UUID>())
+
+    verifyNoInteractions(auth)
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Do not update contact if auth verified`() {
+    script.authUsersMap.putAll(
+      mapOf(
+        "AAA11A" to User(UUID1, "AAA11A", "main1", "EMAIL"),
+        "AAA11B" to User(UUID2, "AAA11B", "main2", "SECONDARY_EMAIL"),
+        "AAA11C" to User(UUID3, "AAA11A", "main3", "TEXT")
+      )
+    )
+    script.authUserContactsMap = mapOf(
+      UUID1 to listOf(
+        Contact(UUID1, "MOBILE_PHONE", "1111", true),
+        Contact(UUID1, "SECONDARY_EMAIL", "email1", true),
+      ),
+      UUID2 to listOf(
+        Contact(UUID2, "MOBILE_PHONE", "2222", true),
+        Contact(UUID2, "SECONDARY_EMAIL", "email2", true),
+      ),
+      UUID3 to listOf(
+        Contact(UUID3, "MOBILE_PHONE", "3333", true),
+        Contact(UUID3, "SECONDARY_EMAIL", "email3", true),
+      ),
+    )
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "sms-email", "1234567", true, true)
+    )
+    script.doMigrate(
+      withCMDResultSet("aaa11b", "sms-email", "1234567", true, true)
+    )
+    script.doMigrate(
+      withCMDResultSet("aaa11c", "sms-email", "1234567", true, true)
+    )
+
+    verify(auth).update("update users set mfa_preference=? where user_id = ?", "SECONDARY_EMAIL", UUID1)
+    // pref of 2nd is already SECONDARY_EMAIL
+    verify(auth).update("update users set mfa_preference=? where user_id = ?", "SECONDARY_EMAIL", UUID3)
+    verifyNoMoreInteractions(auth)
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Do not update contact if email or mob is the same`() {
+    script.authUsersMap.putAll(
+      mapOf(
+        "AAA11A" to User(UUID1, "AAA11A", "main1", "EMAIL"),
+        "AAA11B" to User(UUID2, "AAA11B", "main2", "SECONDARY_EMAIL"),
+        "AAA11C" to User(UUID3, "AAA11A", "main3", "TEXT")
+      )
+    )
+    script.authUserContactsMap = mapOf(
+      UUID1 to listOf(
+        Contact(UUID1, "MOBILE_PHONE", "1111", false),
+        Contact(UUID1, "SECONDARY_EMAIL", "email1", false)
+      ),
+      UUID2 to listOf(
+        Contact(UUID2, "MOBILE_PHONE", "2222", false),
+        Contact(UUID2, "SECONDARY_EMAIL", "email2", false)
+      ),
+      UUID3 to listOf(
+        Contact(UUID3, "MOBILE_PHONE", "3333", false),
+        Contact(UUID3, "SECONDARY_EMAIL", "email3", false),
+      ),
+    )
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email1", "1111", true, true)
+    )
+    script.doMigrate(
+      withCMDResultSet("aaa11b", "email2", "2222", true, true)
+    )
+    script.doMigrate(
+      withCMDResultSet("aaa11c", "email3", "3333", true, true)
+    )
+
+    verify(auth).update("update users set mfa_preference=? where user_id = ?", "SECONDARY_EMAIL", UUID1)
+    // pref of 2nd is already SECONDARY_EMAIL
+    verify(auth).update("update users set mfa_preference=? where user_id = ?", "SECONDARY_EMAIL", UUID3)
+    verifyNoMoreInteractions(auth)
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Insert when nothing in auth tables`() {
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", true, false)
+    )
+
+    verify(auth, never()).update(any<String>(), any<String>(), any<UUID>())
+
+    val uuid = AtomicReference<UUID>()
+    verify(insertIntoUsers).execute(
+      org.mockito.kotlin.check<Map<String, String>> {
+        assertThat(it["username"]).isEqualTo("AAA11A")
+        assertThat(it["mfa_preference"]).isEqualTo("SECONDARY_EMAIL")
+        uuid.set(UUID.fromString(it["user_id"]))
+      }
+    )
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to uuid.get(),
+        "type" to "SECONDARY_EMAIL",
+        "details" to "email2",
+        "verified" to true
+      )
+    )
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to uuid.get(),
+        "type" to "MOBILE_PHONE",
+        "details" to "0123456",
+        "verified" to false
+      )
+    )
+    verifyNoMoreInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Insert when only user in auth tables`() {
+    script.authUsersMap.put("AAA11A", User(UUID1, "AAA11A", "other", "EMAIL"))
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", false, true)
+    )
+
+    verify(auth).update("update users set mfa_preference=? where user_id = ?", "TEXT", UUID1)
+    verify(insertIntoUsers, never()).executeAndReturnKeyHolder(any<Map<String, *>>())
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "SECONDARY_EMAIL",
+        "details" to "email2",
+        "verified" to false
+      )
+    )
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "MOBILE_PHONE",
+        "details" to "0123456",
+        "verified" to true
+      )
+    )
+    verifyNoMoreInteractions(insertIntoUserContact)
+  }
+
+  /**
+   * add or update mobile no, set to TEXT,
+   Add 2ndary email if not present and in CMD
+   */
+  @Test
+  fun `Existing with CMD useSMS update with email`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf(
+      UUID1 to listOf(
+        Contact(UUID1, "MOBILE_PHONE", "9999", false),
+        Contact(UUID1, "SECONDARY_EMAIL", "other", false),
+      )
+    )
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", false, true)
+    )
+
+    verify(auth).update(UPDATE_USER_CONTACT, "0123456", UUID1, "MOBILE_PHONE")
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "TEXT", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Existing with CMD useSMS update, no email`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf(
+      UUID1 to listOf(
+        Contact(UUID1, "MOBILE_PHONE", "9999", false),
+        Contact(UUID1, "SECONDARY_EMAIL", "other", false),
+      )
+    )
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", null, "0123456", false, true)
+    )
+
+    verify(auth).update(UPDATE_USER_CONTACT, "0123456", UUID1, "MOBILE_PHONE")
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "TEXT", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Existing with CMD useSMS insert`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", false, true)
+    )
+
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "SECONDARY_EMAIL",
+        "details" to "email2",
+        "verified" to false
+      )
+    )
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "MOBILE_PHONE",
+        "details" to "0123456",
+        "verified" to true
+      )
+    )
+    verifyNoMoreInteractions(insertIntoUserContact)
+
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "TEXT", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+  }
+
+  /**
+   * add or update 2ndary email, set to SECONDARY_EMAIL,
+   Add no if not present and in CMD
+   */
+  @Test
+  fun `Existing with CMD useEmailAddress update`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf(
+      UUID1 to listOf(
+        Contact(UUID1, "MOBILE_PHONE", "9999", false),
+        Contact(UUID1, "SECONDARY_EMAIL", "other", false),
+      )
+    )
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", true, false)
+    )
+
+    verify(auth).update(UPDATE_USER_CONTACT, "email2", UUID1, "SECONDARY_EMAIL")
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "SECONDARY_EMAIL", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+    verifyNoInteractions(insertIntoUserContact)
+  }
+
+  @Test
+  fun `Existing with CMD useEmailAddress insert with sms`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", "0123456", true, false)
+    )
+
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "SECONDARY_EMAIL",
+        "details" to "email2",
+        "verified" to true
+      )
+    )
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "MOBILE_PHONE",
+        "details" to "0123456",
+        "verified" to false
+      )
+    )
+    verifyNoMoreInteractions(insertIntoUserContact)
+
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "SECONDARY_EMAIL", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+  }
+
+  @Test
+  fun `Existing with CMD useEmailAddress insert, no sms`() {
+    script.authUsersMap.putAll(
+      mapOf("AAA11A" to User(UUID1, "AAA11A", "email1", "EMAIL"))
+    )
+    script.authUserContactsMap = mapOf()
+
+    script.doMigrate(
+      withCMDResultSet("aaa11a", "email2", null, true, false)
+    )
+
+    verify(insertIntoUserContact).execute(
+      mapOf(
+        "user_id" to UUID1,
+        "type" to "SECONDARY_EMAIL",
+        "details" to "email2",
+        "verified" to true
+      )
+    )
+    verifyNoMoreInteractions(insertIntoUserContact)
+
+    verify(auth).update(UPDATE_MFA_PREFERENCE, "SECONDARY_EMAIL", UUID1)
+    verifyNoMoreInteractions(auth)
+
+    verifyNoInteractions(insertIntoUsers)
+  }
+
+  private fun withCMDResultSet(
+    quantumId: String,
+    emailAddress: String?,
+    sms: String?,
+    useEmailAddress: Boolean,
+    useSms: Boolean,
+  ): ResultSet {
+    val cmdResult: ResultSet = mock()
+    whenever(cmdResult.getString("QuantumId")).thenReturn(quantumId)
+    whenever(cmdResult.getString("EmailAddress")).thenReturn(emailAddress)
+    whenever(cmdResult.getString("Sms")).thenReturn(sms)
+    whenever(cmdResult.getBoolean("UseEmailAddress")).thenReturn(useEmailAddress)
+    whenever(cmdResult.getBoolean("UseSms")).thenReturn(useSms)
+    return cmdResult
+  }
+}
