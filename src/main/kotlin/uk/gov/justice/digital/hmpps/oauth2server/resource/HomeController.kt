13c30356103804ba5df903459a150658adc310f0
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2022-05-25 13:32:37 +0100

    HAAR-477 Pick up evaluated authorities for rolecount check and fix typo (#1294)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/HomeController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/HomeController.kt
@@ -26,7 +26,7 @@ class HomeController(
 
     val services = authServicesService.listEnabled(authorities)
 
-    return ModelAndView("landing", "services", services).addObject("userRoleCount", authentication.authorities.size)
+    return ModelAndView("landing", "services", services).addObject("userRoleCount", authorities.size)
   }
 
   @GetMapping("/terms")
