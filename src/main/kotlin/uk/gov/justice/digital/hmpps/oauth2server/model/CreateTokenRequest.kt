44b73b0a4767450fc0f81b9392c67b68919947b7
ravmoj <104509282+ravmoj@users.noreply.github.com>
2022-07-04 16:59:03 +0100

    HAAR-605: Added swagger docs create new token endpoint (#1327)
    * HAAR-588: Added token reset endpoint.
    
    * HAAR-588: Added missing file
    
    * HAAR-588: Updated tests
    
    * HAAR-588: Updated tests with respect to new role in flyway script (MAINTAIN_ACCESS_ROLES_ADMIN)
    
    * HAAR-588: Updated tests with respect to new role in flyway script (MAINTAIN_ACCESS_ROLES_ADMIN)
    
    * HAAR-588: Updated role 'ROLE_CREATE_EMAIL_TOKEN'
    
    * HAAR-605: Added swagger to '/api/new-token'
    
    * HAAR-605: Added swagger to '/api/new-token' endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/model/CreateTokenRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/model/CreateTokenRequest.kt
@@ -1,14 +1,33 @@
 package uk.gov.justice.digital.hmpps.oauth2server.model
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Schema(description = "Token creation request")
+@Schema(description = "Token creation  request")
+@ApiModel(description = "Request to create Token")
 data class CreateTokenRequest(
-  @NotBlank val username: String,
+  @ApiModelProperty(required = true, value = "Username", example = "DEMO_USER1", position = 1)
+  @Schema(description = "Username", example = "DEMO_USER1", required = true) @field:Size(
+    max = 30,
+    min = 1,
+    message = "username must be between 1 and 30"
+  ) @NotBlank val username: String,
+
+  @ApiModelProperty(required = true, value = "Email", example = "john.smith@digital.justice.gov.uk", position = 2)
+  @Schema(description = "DPS Email Address of user", example = "test@justice.gov.uk", required = true)
+  @field:Email(
+    message = "Not a valid email address"
+  )
   @NotBlank val email: String,
+
+  @ApiModelProperty(required = true, value = "AuthSource", example = "auth or azuread or delius or nomis or none", position = 3)
+  @Schema(description = "AuthSource should be either nomis or delius", example = "nomis", required = true)
   @NotBlank val source: AuthSource,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/TokenController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/TokenController.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.oauth2server.resource.api
 
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.oauth2server.auth.model.UserToken
 import uk.gov.justice.digital.hmpps.oauth2server.model.CreateTokenRequest
+import uk.gov.justice.digital.hmpps.oauth2server.model.ErrorDetail
 import uk.gov.justice.digital.hmpps.oauth2server.verify.TokenService
+import javax.validation.Valid
 
 @RestController
 @Validated
@@ -15,8 +20,19 @@ class TokenController(private val tokenService: TokenService) {
 
   @PreAuthorize("hasRole('ROLE_CREATE_EMAIL_TOKEN')")
   @PostMapping("/api/new-token")
+  @ApiOperation(
+    value = "Generates new token for DPS user",
+    notes =
+    """
+      Generates new token for DPS user.  Requires ROLE_CREATE_EMAIL_TOKEN.
+    """,
+    nickname = "createNewTokenByUserName",
+    consumes = "application/json",
+    produces = "text/plain;charset=UTF-8"
+  )
+  @ApiResponses(value = [ApiResponse(code = 401, message = "Unauthorized.", response = ErrorDetail::class)])
   fun createNewTokenByUserName(
-    @RequestBody createTokenRequest: CreateTokenRequest
+    @Valid @RequestBody createTokenRequest: CreateTokenRequest
   ): String {
     return tokenService.createTokenForNewUser(
       UserToken.TokenType.RESET,
